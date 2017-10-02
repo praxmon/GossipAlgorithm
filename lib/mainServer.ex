57f648ff3d6bd09ac0d077ea256bb708d2cffb09
Prakhar Mohan Srivastava <prakharmohan@ufl.edu>
2017-10-02 15:37:30 -0400

    Adding the main server file, logic to be added after discussion

---
+++ lib/mainServer.ex
@@ -0,0 +1,14 @@
+defmodule MainServer do
+    use GenServer
+
+    def start_link(opts) do
+        #parse arguments if necessary
+    end
+
+    def handle_call() do
+        #handle call if necessary
+    end
+
+    def handle_cast() do
+        #handle cast if necessary
+    end
